f090fd40a4733c598837ff47e00458fe1f9c38c7
ZainapKurbanova <160218230+ZainapKurbanova@users.noreply.github.com>
2025-09-25 13:33:40 +0300

    Update Preparable.cs

--- Laba8var/interfaces/Preparable.cs
+++ Laba8var/interfaces/Preparable.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Laba8var.Models
+namespace Laba8var.Interfaces
 {
     /// <summary>
     /// Интерфейс, описывающий объекты, которые можно приготовить.
